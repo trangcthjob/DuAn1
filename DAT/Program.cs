61cd32d91bf2b92edb8f43f11b1f3cb1ca0e29fa
longtrinhduy <longtrinh3608@gmail.com>
2023-11-22 19:19:08 +0700

    Form Tài khoản

--- DAT/Program.cs
+++ DAT/Program.cs
@@ -11,7 +11,7 @@ namespace DAT
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FormCa());
+            Application.Run(new FormTaiKhoan());
         }
     }
 }
